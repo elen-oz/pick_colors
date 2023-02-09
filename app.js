f78fcb624909d87e312517fb61ccec3d39aa55de
Elena Ozerova <ozerova.lenka@gmail.com>
2023-02-09 16:55:00 +0100

    add changing lockers

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const cols = document.querySelectorAll('.col');
 
 document.addEventListener('keydown', (event) => {
+  event.preventDefault();
   if (event.code.toLocaleLowerCase() === 'space') {
     setRandomColors();
   }
 });
 
-function generateRandomColor() { 
+document.addEventListener('click', (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === 'lock') {
+    const node =
+      event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0];
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
+  }
+});
+
+function generateRandomColor() {
   const hexCodes = '0123456789ABCDEF';
   let color = '';
   for (let i = 0; i < 6; i++) {
