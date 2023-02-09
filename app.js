7e414b4b02f172e4bb2f316ded733a1def46ea47
Elena Ozerova <ozerova.lenka@gmail.com>
2023-02-09 16:22:24 +0100

    change js

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const cols = document.querySelectorAll('.col');
 
+function generateRandomColor() {
+  const hexCodes = '0123456789ABCDEF';
+  let color = '';
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return '#' + color;
+}
+
 function setRandomColors() {
-  cols.forEach(col => {
-    console.log(col);
-  })
+  cols.forEach((col) => {
+    const text = col.querySelector('h2');
+    const color = generateRandomColor();
+
+    text.textContent = color;
+    col.style.background = generateRandomColor();
+  });
 }
 
 setRandomColors();
